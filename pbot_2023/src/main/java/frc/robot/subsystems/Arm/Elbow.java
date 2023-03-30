eb99d4faecb870aaac4b2bc531f551e75a755d96
wetyuiopsu <51488420+wetyuiopsu@users.noreply.github.com>
2023-03-29 19:57:04 -0500

    added settarget

--- pbot_2023/src/main/java/frc/robot/subsystems/Arm/Elbow.java
+++ pbot_2023/src/main/java/frc/robot/subsystems/Arm/Elbow.java
@@ -8,18 +8,22 @@ import com.revrobotics.CANSparkMax.SoftLimitDirection;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants;
 import frc.robot.Constants.ArmConstants.ElbowConstants;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 
-public class Elbow {
+public class Elbow extends SubsystemBase{
     public CANSparkMax LeftElbowMotor;
     public CANSparkMax RightElbowMotor;
     public AbsoluteEncoder ElbowEncoder;
     public ProfiledPIDController ElbowController;
     private ArmFeedforward elbowFF;
+    private Constraints FarConstraints = new Constraints(12, 9);
+    private Constraints CloseConstraints = new Constraints(36, 36);
     public Elbow(){
         LeftElbowMotor.follow(RightElbowMotor, false);
         LeftElbowMotor = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -44,13 +48,25 @@ public class Elbow {
     public double getAngle(){
         return convertTicksToAngle(ElbowEncoder.getPosition());
     }
-    public void setPos(double goal){
-        ElbowController.setGoal(goal);
+    public void setPos(double goal){	
+        if(ElbowController.getP() == 0) {ElbowController.setP(5);}	
+        ElbowController.setGoal(goal);	
     }
-    public void setAngle(){
-        RightElbowMotor.setVoltage(ElbowController.calculate(getAngle(),
-        ElbowController.getGoal()));
-        elbowFF.calculate(ElbowController.getSetpoint().position, 0);
+    public boolean atGoal() {	
+        return ElbowController.atGoal();	
+    }
+    public void setTargetAngle(double targetAngle){
+        Constraints selectedConstraint = (Math.abs(targetAngle - getAngle()) > Units.degreesToRadians(10) ? FarConstraints : CloseConstraints);
+        ElbowController.setConstraints(selectedConstraint);
+
+        ElbowController.setGoal(new State(targetAngle, 0));
+    }
+    public void setCalculatedVoltage(){	
+        RightElbowMotor.setVoltage(ElbowController.calculate(getAngle(),ElbowController.getGoal()) + elbowFF.calculate(ElbowController.getSetpoint().position, 0));	
+    }	
+    @Override	
+    public void periodic() {	
+    setCalculatedVoltage();	
     }
 }
 
--- pbot_2023/src/main/java/frc/robot/subsystems/Arm/Shoulder.java
+++ pbot_2023/src/main/java/frc/robot/subsystems/Arm/Shoulder.java
@@ -7,14 +7,21 @@ import com.revrobotics.CANSparkMax.IdleMode;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ArmConstants.ShoulderConstants;
+import frc.robot.subsystems.Arm.ArmStateMachine.ArmState;
 
-public class Shoulder {
+public class Shoulder extends SubsystemBase{
     public CANSparkMax LeftShoulderMotor;
     public CANSparkMax RightShoulderMotor;
     public AbsoluteEncoder ShoulderEncoder;
     public ProfiledPIDController ShoulderController;
     private ArmFeedforward shoulderFF;
+    private Constraints FarConstraints = new Constraints(10, 8);
+    private Constraints CloseConstraints = new Constraints(18, 18);
     public Shoulder(){
         LeftShoulderMotor.follow(RightShoulderMotor, false);
         LeftShoulderMotor = new CANSparkMax(0, CANSparkMaxLowLevel.MotorType.kBrushless);
@@ -31,21 +38,33 @@ public class Shoulder {
         shoulderFF = new ArmFeedforward(0,0.47,4.68,0.04);
         ShoulderEncoder.setZeroOffset(ShoulderConstants.kShoulderEncoderZeroOffset);
     }
+    public void setPos(double goal){
+        if(ShoulderController.getP() == 0) {ShoulderController.setP(5);}
+        ShoulderController.setGoal(goal);
+    }
     public double convertTicksToAngle(double angle){
         double newAngle = angle;
         newAngle -= ShoulderConstants.kShoulderEncoderZeroOffset;
         return newAngle / ShoulderConstants.kShoulderGearRatio;
     }
+    public boolean atGoal() {
+        return ShoulderController.atGoal();
+    }
     public double getAngle(){
         return convertTicksToAngle(ShoulderEncoder.getPosition());
     }
-    public void setPos(double goal){
-        ShoulderController.setGoal(goal);
+    public void setTargetAngle(double targetAngle){
+        Constraints selectedConstraint = (Math.abs(targetAngle - getAngle()) > Units.degreesToRadians(10) ? FarConstraints : CloseConstraints);
+        ShoulderController.setConstraints(selectedConstraint);
+
+        ShoulderController.setGoal(new State(targetAngle, 0));
     }
-    public void setAngle(){
-        RightShoulderMotor.setVoltage(ShoulderController.calculate(getAngle(),
-        ShoulderController.getGoal()));
-        shoulderFF.calculate(ShoulderController.getSetpoint().position, 0);
+    public void setCalculatedVoltage(){
+        RightShoulderMotor.setVoltage(ShoulderController.calculate(getAngle(),ShoulderController.getGoal()) + shoulderFF.calculate(ShoulderController.getSetpoint().position, 0));
+    }
+    @Override
+    public void periodic() {
+    setCalculatedVoltage();
     }
 }
 
